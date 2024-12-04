95439b1fc09f161ce9467c226612c7729c7682d3
noahOU <157321555+noahOU@users.noreply.github.com>
2024-12-04 14:23:05 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('mis-4013.mysql.database.azure.com', 'User', '!Thunder935', 'oufootball');
+    $conn = new mysqli('mis-4013.mysql.database.azure.com', 'User', '!Thunder935', 'ou_football');
     
     // Check connection
     if ($conn->connect_error) {
