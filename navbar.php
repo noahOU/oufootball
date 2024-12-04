bb30f1679ec190040f532bcfe5937c715b236c46
noahOU <157321555+noahOU@users.noreply.github.com>
2024-12-04 14:42:09 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,22 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg" style="background-color: #841617;">
     <div class="container">
-        <a class="navbar-brand" href="index.php">OU Football Hub</a>
+        <a class="navbar-brand text-white" href="index.php">OU Football Hub</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link text-white" href="index.html">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="roster.php">Roster</a>
+                    <a class="nav-link text-white" href="roster.php">Roster</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="schedule.php">Schedule</a>
+                    <a class="nav-link text-white" href="schedule.php">Schedule</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="fans.php">Fans</a>
+                    <a class="nav-link text-white" href="fans.php">Fans</a>
                 </li>
             </ul>
         </div>
