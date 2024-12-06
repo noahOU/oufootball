b8d5072ef4007bed2fbad607ac1a759f27f835de
noahOU <157321555+noahOU@users.noreply.github.com>
2024-12-05 18:54:09 -0600

    Update transfer-portal.php

--- transfer-portal.php
+++ transfer-portal.php
@@ -6,6 +6,7 @@
     <title>Transfer Portal</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="icon" type="image/png" href="images/helmet.png">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php require 'navbar.php'; ?>
