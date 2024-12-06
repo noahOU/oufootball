bdde788bf86e2b749ed1df0f7cbeaf84cdee6bdf
noahOU <157321555+noahOU@users.noreply.github.com>
2024-12-05 18:32:40 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,24 +1,28 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
 <nav class="navbar navbar-expand-lg" style="background-color: #841617;">
     <div class="container">
-        <a class="navbar-brand text-white" href="index.html">OU Football Hub</a>
+        <a class="navbar-brand text-white" href="index.php">OU Football Hub</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="index.html">Home</a>
+                    <a class="nav-link <?php echo $current_page == 'index.php' ? 'active text-warning' : 'text-white'; ?>" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="roster.php">Roster</a>
+                    <a class="nav-link <?php echo $current_page == 'roster.php' ? 'active text-warning' : 'text-white'; ?>" href="roster.php">Roster</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="schedule.php">Schedule</a>
+                    <a class="nav-link <?php echo $current_page == 'schedule.php' ? 'active text-warning' : 'text-white'; ?>" href="schedule.php">Schedule</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="fans.php">Fans</a>
+                    <a class="nav-link <?php echo $current_page == 'fans.php' ? 'active text-warning' : 'text-white'; ?>" href="fans.php">Fans</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
